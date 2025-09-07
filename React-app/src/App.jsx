3d16e293bd7de5005ddcdc5abe4193f4b678b70a
Sharad Bista <bishalbista737@gmail.com>
2025-09-07 08:58:29 +0545

    Adding my first react project

---
+++ React-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import Compo from "./component/example1/Compo";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-blue-400">Hello</h1>
+      <Compo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React-app/src/component/example1/Compo.jsx
@@ -0,0 +1,22 @@
+//Method 1 to export this Component file to main.jsx or App.jsx files
+
+// export const Compo = () => {
+//     return (
+//         <div>
+//             <h1>
+//                 Hello, I am a Component.
+//             </h1>
+//         </div>
+//     )
+// }
+
+// Method 2 to export this Component file to main.jsx or App.jsx files
+
+const Compo = () => {
+    return (
+        <div>
+            <h1>Hello, I am a Component.</h1>
+        </div>
+    )
+}
+export default Compo;
